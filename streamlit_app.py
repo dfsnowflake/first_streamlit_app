de590cb6429126e6e42ac79e31d80b6c18db73e2
dfsnowflake <123736897+dfsnowflake@users.noreply.github.com>
2023-02-01 12:47:46 +0100

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import request
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -22,6 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#New Section to display fruitvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
